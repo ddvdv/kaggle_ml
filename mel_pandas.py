f88fd9eae820d6b13d3675345bbc8779fd5732d7
davidv <david.vandervaeren@police.belgium.eu>
2019-01-20 16:07:58 +0100

    randomforest

--- mel_pandas.py
+++ mel_pandas.py
@@ -2,6 +2,7 @@
 
 import pandas as pd
 from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import train_test_split
 
@@ -14,13 +15,18 @@ X = melbourne_data[melbourne_features]
 
 train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=0)
 
-def get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y):
-    model = DecisionTreeRegressor(max_leaf_nodes=max_leaf_nodes, random_state=0)
-    model.fit(train_X, train_y)
-    preds_val = model.predict(val_X)
-    mae = mean_absolute_error(val_y, preds_val)
-    return(mae)
+#def get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y):
+#    model = DecisionTreeRegressor(max_leaf_nodes=max_leaf_nodes, random_state=0)
+#    model.fit(train_X, train_y)
+#    preds_val = model.predict(val_X)
+#    mae = mean_absolute_error(val_y, preds_val)
+#    return(mae)
+#
+#for max_leaf_nodes in [5, 50, 500, 5000]:
+#    my_mae = get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y)
+#    print("Max leaf nodes: %d \t\t Mean Absolute Error: %d" % (max_leaf_nodes, my_mae))
 
-for max_leaf_nodes in [5, 50, 500, 5000]:
-    my_mae = get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y)
-    print("Max leaf nodes: %d \t\t Mean Absolute Error: %d" % (max_leaf_nodes, my_mae))
+forest_model = RandomForestRegressor(random_state=1)
+forest_model.fit(train_X, train_y)
+melb_preds = forest_model.predict(val_X)
+print(mean_absolute_error(val_y, melb_preds))
